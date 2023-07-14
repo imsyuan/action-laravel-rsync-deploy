44d7416b21d35f250633d11b521b12ed65b68db6
steven.chang <steven.chang@104.com.tw>
2023-07-14 17:54:55 +0800

    chore: update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ mkdir "$SSHPATH"
 echo "$SSH_PRIVATE_KEY" > "$SSHPATH/key"
 echo "Host *
               PubkeyAcceptedKeyTypes +ssh-rsa
+              KexAlgorithms +diffie-hellman-group1-sha1
               HostKeyAlgorithms +ssh-rsa" > "$SSHPATH/config"
 chmod 400 "$SSHPATH/key"
 chmod 400 "$SSHPATH/config"
