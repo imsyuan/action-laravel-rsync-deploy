4284287fcecf54389c12086445c2cdf278fd2f1e
steven.chang <steven.chang@104.com.tw>
2023-07-14 15:33:42 +0800

    chore: update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,16 @@ mkdir "$SSHPATH"
 echo "$SSH_PRIVATE_KEY" > "$SSHPATH/key"
 chmod 400 "$SSHPATH/key"
 SERVER_DEPLOY_STRING="$REMOTE_USER@$REMOTE_HOST:$TARGET_DIRECTORY"
-rm known_hosts
+
+
+file_path"$SSHPATH/known_hosts"
+
+if test -f "$file_path"; then
+    rm "$SSHPATH/known_hosts"
+    echo "File deleted."
+else
+    echo "File does not exist."
+fi
+
 # Run Rsync synchronization
 sh -c "rsync $ARGS -e 'ssh -i $SSHPATH/key -o PubkeyAcceptedKeyTypes=+ssh-rsa -o HostKeyAlgorithms=+ssh-rsa -p $REMOTE_HOST_PORT' . $SERVER_DEPLOY_STRING"
